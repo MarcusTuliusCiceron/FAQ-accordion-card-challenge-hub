2998e48e8a244507710d8c4ef9b80854588997c3
Ciceron <100491130+MarcusTuliusCiceron@users.noreply.github.com>
2022-03-04 20:55:38 +0100

    working half animated

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 document.querySelectorAll('.accordion__item__question').forEach(item =>{
     item.addEventListener('click', event => {
-        console.log('click');
-        if (item.classList.contains('active')){
-            item.classList.remove('active')
-            item.nextElementSibling.classList.add('hidden')
-        }
-        else{
-            item.classList.add('active')
-            item.nextElementSibling.classList.remove('hidden')
-        }
-        
+        item.classList.toggle('active');
+        item.nextElementSibling.classList.toggle('hidden');
     })
 })
